06cc3191f02350760b8b51bc8025a6bd8dad405a
Viny Araujo <v.araujo1989@gmail.com>
2023-01-06 19:20:05 -0300

    Ajustes forca

--- src/main.rs
+++ src/main.rs
@@ -46,9 +46,12 @@ fn main() {
             println!("\x1b[94m\n\nVoce tem {} turnos restantes.\x1b[0m",turns_left);
         }
         
+        //Mostra progresso EX: _ _ _ _ _
         display_progress(&letters);
         
         println!("\nEntre com o palpite da sua letra: ");
+
+        //Le o INPUT do usuario
         let user_char = read_user_input_character();
 
         //Sair se o usuario digitar um *
@@ -90,12 +93,12 @@ fn main() {
             }
         }
     }
-    
     println!("\x1b[97mAte mais!\n\n\n\x1b[0m");
 }
 
 //MAIN FIM ===============================================================================
 
+//Escolhe a palavra da lista de palavras
 fn select_word() -> String{
     //Abrir arquivo
     let mut file = File::open("words.txt").expect("Falha ao abrir o arquivo!");
@@ -113,6 +116,7 @@ fn select_word() -> String{
     return String::from(avaliable_words[random_index]);
 }
 
+//Separa a palavras em vetor de caracteres
 fn create_letters(word: &String) -> Vec<Letter>{
     //Cria vetor vazio
     let mut letters: Vec<Letter> = Vec::new();
@@ -127,6 +131,7 @@ fn create_letters(word: &String) -> Vec<Letter>{
 return letters;
 }
 
+//Mostra o progresso do jogo
 fn display_progress(letters: &Vec<Letter>){
     let mut display_string = String::from("Progress: ");
 
@@ -145,6 +150,7 @@ fn display_progress(letters: &Vec<Letter>){
     println!("{}", display_string);
 }
 
+//Le o input do usuario
 fn read_user_input_character() -> char{
     let mut user_input = String::new();
     match io::stdin().read_line(&mut user_input){
@@ -158,6 +164,7 @@ fn read_user_input_character() -> char{
     }
 }
 
+//Checa o progresso do jogo e informa no caso de progresso, derrota, ou vitoria
 fn check_progress(turns_left: u8, letters: &Vec<Letter>) -> GameProgress {
     //Checa se todas as letras foram reveladas
     let mut all_revealed = true;
